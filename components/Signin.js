939e63ba49588bf5ca4764aa7f7df149e854d923
CHAERYEON-LEE <sogogijjang@naver.com>
2022-10-11 19:43:29 +0900

    feat: create Signin component and add style
    - 로그인 컴포넌트 완성
    - 로그인 css 스타일 완성

--- components/Signin.js
+++ components/Signin.js
@@ -1,11 +1,27 @@
 import Component from './Component.js';
 
 class Signin extends Component {
-  addEvent() {
-    return;
-  }
+  // addEvent() {}
 
-  domStr() {}
+  domStr() {
+    return `<div class="signin-container">
+              <div class="signin-title">SIGN IN</div>
+              <form class="form signin_info" novalidate>
+                <div class="input-container">
+                  <label for="signin-userid">ID:</label>
+                  <input type="text" id="signin-userid" name="userid" required autocomplete="off" />
+                  <div class="error"></div>
+                </div>
+                <div class="input-container">
+                  <label for="signin-password">Password:</label>
+                  <input type="password" id="signin-password" name="password" required autocomplete="off" />
+                  <div class="error"></div>
+                </div>
+                <a class="signup-btn" href="/">SIGN UP</a>
+                <button class="signin-btn">LOGIN</button>
+              </form>
+            </div>`;
+  }
 }
 
 export default Signin;
