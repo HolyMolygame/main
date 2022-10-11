f48415be93e704e1b76567472a7424fda226629f
loco9939 <kls9939@naver.com>
2022-10-11 20:07:43 +0900

    style: Modify styles
    - header 중앙 정렬
    - play 버튼 호버 기능
    - profile border 그래디언트 추가

--- components/Contributor.js
+++ components/Contributor.js
@@ -19,13 +19,13 @@ class Contributor extends Component {
         </div>
       </div>
       <div class="profile">
-        <img src="./src/img/jiyoon_profile.webp" alt="orange 프로필" />
         <div class="profile-description">
           <p class="profile-title">name : kim</p>
           <p class="profile-title">hobby : bicycle</p>
           <p class="profile-title">git-hub : https://github.com/loco9939 </p>
           <p class="profile-title">Motto : We all live in gutter. But some of us are looking at the stars.</p>
         </div>
+      <img src="./src/img/jiyoon_profile.png" alt="orange 프로필" />
       </div>
       <div class="profile">
         <img src="./src/img/charyeon_profile.webp" alt="chae 프로필" />
