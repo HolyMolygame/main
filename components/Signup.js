a00c245a7b9cd4dd1ce7f236d69abd08638eae5c
CHAERYEON-LEE <sogogijjang@naver.com>
2022-10-11 20:39:31 +0900

    feat: create signup component and add signup style
    - signup 자바스크립트 코드 완성
    - signup css style 완성

--- components/Signup.js
+++ components/Signup.js
@@ -1,11 +1,37 @@
 import Component from './Component.js';
 
 class Signup extends Component {
-  addEvent() {
-    return;
-  }
+  // addEvent() {}
 
-  domStr() {}
+  domStr() {
+    return `<div class="signup-container">
+              <div class="signup-title">SIGN UP</div>
+              <form class="form signup_info" novalidate>
+                <div class="input-container">
+                  <label for="signup-userid">NICKNAME:</label>
+                  <input type="text" id="signup-userid" name="userid" required autocomplete="off" />
+                </div>
+                <div class="hidden error">IS ALREADY REGISTERED</div>
+                <div class="input-container">
+                  <label for="signup-name">ID:</label>
+                  <input type="text" id="signup-name" name="username" required autocomplete="off" />
+                </div>
+                <div class="hidden error">INVALID ID. PLEASE CHECK YOUR ID</div>
+                <div class="input-container">
+                  <label for="signup-password">PASSWORD:</label>
+                  <input type="password" id="signup-password" name="password" required autocomplete="off" />
+                </div>
+                <div class="hidden error">INVALID PW. PLEASE CHECK YOUR PW</div>
+                <div class="input-container">
+                  <label for="signup-confirm-password">CONFIRM:</label>
+                  <input type="password" id="signup-confirm-password" name="confirm-password" required autocomplete="off" />
+                </div>
+                <div class="hidden error">PLEASE CHECK YOUR PW</div>
+                <button class="submit-btn" disabled>SUBMIT</button>
+              </form>
+            </div>
+    `;
+  }
 }
 
 export default Signup;
