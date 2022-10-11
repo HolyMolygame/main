8612e1ed527bbedf43eb6afb8debc078086192c3
loco9939 <kls9939@naver.com>
2022-10-11 17:33:28 +0900

    feat: Create GameCard HTML,CSS

--- components/GameCard.js
+++ components/GameCard.js
@@ -1,11 +1,19 @@
 import Component from './Component.js';
 
 class GameCard extends Component {
-  addEvent() {
-    return;
-  }
+  // addEvent() {
+  //   return;
+  // }
 
-  domStr() {}
+  domStr() {
+    return `
+    <div class="game-card">
+      <h2 class="game-card-heading">MATCHING CARDS</h2>
+      <p>remember cards and match them</p>
+      <button class="game-card-play">play</button>
+    </div>
+    `;
+  }
 }
 
 export default GameCard;
