14468c01c1e0adb67dfd3b9ed411a38ae5957677
OrangeChick <retww@naver.com>
2022-10-11 17:56:45 +0900

    feat: Create Rank component and Add style
    - Rank component를 생성하고, 그에 맞는 스타일을 추가하였습니다.

--- components/Rank.js
+++ components/Rank.js
@@ -1,11 +1,78 @@
 import Component from './Component.js';
 
 class Rank extends Component {
-  addEvent() {
-    return;
-  }
+  // addEvent() {
+  //   return;
+  // }
 
-  domStr() {}
+  domStr() {
+    return `
+      <h1 class="rank-title">RANKING <span>TOP 10</span></h1>
+      <div class="rank-container">
+        <table class="rank-table">
+          <thead class="rank-container-head">
+            <tr>
+              <th>RANK</th>
+              <th>NICKNAME</th>
+              <th>SCORE</th>
+            </tr>
+          </thead>
+          <tbody class="rank-container-body">
+            <tr class="rank-first">
+              <td>1</td>
+              <td>YIJU</td>
+              <td>3.1415</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>JUNY</td>
+              <td>3.1415</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>DLCHAE</td>
+              <td>3.1415</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>YIJU</td>
+              <td>3.1415</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>JUNY</td>
+              <td>3.1415</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>DLCHAE</td>
+              <td>3.1415</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>YIJU</td>
+              <td>3.1415</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>JUNY</td>
+              <td>3.1415</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>DLCHAE</td>
+              <td>3.1415</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>YIJU</td>
+              <td>3.1415</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    `;
+  }
 }
 
 export default Rank;
