8b4c88040b3b67c378ecc2b1b83ac15bf69892fe
loco9939 <kls9939@naver.com>
2022-10-12 11:29:47 +0900

    feat: Create custom events
    - 커스텀 이벤트 생성하여 조건에 따라 컴포넌트 재렌더링 일어나도록 구현

--- App.js
+++ App.js
@@ -1,13 +1,90 @@
 import Component from './components/Component.js';
-import { Header, Contributor, GameCard, Footer } from './components/index.js';
+import { Header, Contributor, GameCard, Footer, MatchingCards, Signin, Rank, Signup } from './components/index.js';
 
+let gameplay = false;
+let signin = false;
+let signup = false;
+let rank = false;
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
+  addEvent() {
+    return [
+      this.createEvent({
+        type: 'gameplay',
+        selector: '',
+        handler: e => {
+          if (e.detail) {
+            gameplay = true;
+          }
+        },
+      }),
+      this.createEvent({
+        type: 'signin',
+        selector: '',
+        handler: e => {
+          if (e.detail) {
+            signin = true;
+          }
+        },
+      }),
+      this.createEvent({
+        type: 'rank',
+        selector: '',
+        handler: e => {
+          if (e.detail) {
+            rank = true;
+          }
+        },
+      }),
+      this.createEvent({
+        type: 'signup',
+        selector: '',
+        handler: e => {
+          if (e.detail) {
+            signup = true;
+          }
+        },
+      }),
+    ];
+  }
+
   domStr() {
+    if (gameplay) {
+      gameplay = false;
+      return `
+      ${new Header().domStr()}
+      ${new MatchingCards().domStr()}
+      ${new Footer().domStr()}
+      `;
+    }
+    if (signin) {
+      signin = false;
+      return `
+      ${new Header().domStr()}
+      ${new Signin().domStr()}
+      ${new Footer().domStr()}
+      `;
+    }
+    if (signup) {
+      signup = false;
+      return `
+      ${new Header().domStr()}
+      ${new Signup().domStr()}
+      ${new Footer().domStr()}
+      `;
+    }
+    if (rank) {
+      rank = false;
+      return `
+      ${new Header().domStr()}
+      ${new Rank().domStr()}
+      ${new Footer().domStr()}
+      `;
+    }
     return `
       ${new Header().domStr()}
       ${new GameCard().domStr()}
