33d07745e76ba6e6f1ea9264bbd4fb87d2d5f6bd
loco9939 <kls9939@naver.com>
2022-10-12 11:28:45 +0900

    feat: Create event
    - 회원가입으로 이동하는 이벤트 생성

--- components/Signin.js
+++ components/Signin.js
@@ -1,7 +1,28 @@
 import Component from './Component.js';
 
+const signup = new CustomEvent('signup', {
+  detail: true,
+});
 class Signin extends Component {
-  // addEvent() {}
+  addEvent() {
+    //   this.createEvent({
+    //     type:'click',
+    //     selector:'.signin-btn',
+    //     handler: e => {
+    //       // input 값 유효성 검사
+    //       // 이벤트 생성하기 전에 login 상태관리부터 선행되어야 할듯
+    //     }
+    //   })
+    return [
+      this.createEvent({
+        type: 'click',
+        selector: '.signup-btn',
+        handler: () => {
+          window.dispatchEvent(signup);
+        },
+      }),
+    ];
+  }
 
   domStr() {
     return `<div class="signin-container">
@@ -17,7 +38,7 @@ class Signin extends Component {
                   <input type="password" id="signin-password" name="password" required autocomplete="off" />
                 </div>
                 <div class="hidden error">PASSWORDS DO NOT MATCH.</div>
-                <a class="signup-btn" href="/">SIGN UP</a>
+                <button type="button" class="signup-btn">SIGN UP</button>
                 <button class="signin-btn">LOGIN</button>
               </form>
             </div>`;
