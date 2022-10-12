2ee87f17da7edd5d81c198ef2fe5223ffaa203f1
loco9939 <kls9939@naver.com>
2022-10-12 16:43:53 +0900

    feat: Create server.js

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
 import App from './App.js';
 import render from './render/index.js';
 
+window.history.pushState(null, null, '/');
 render(App, document.getElementById('root'));
 
-window.addEventListener('gameplay', () => {
-  render();
-});
-window.addEventListener('signin', () => {
-  render();
-});
-window.addEventListener('signup', () => {
-  render();
-});
-window.addEventListener('main', () => {
-  render();
-});
-window.addEventListener('rank', () => {
-  render();
-});
