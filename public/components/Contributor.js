57c1aa376d58f25ac80f6cf9524ec02cf57be145
loco9939 <kls9939@naver.com>
2022-10-18 22:13:47 +0900

    style: Modify contributor style
    - 인적사항 수정 및 스타일 변경

--- public/components/Contributor.js
+++ public/components/Contributor.js
@@ -8,28 +8,28 @@ class Contributor extends Component {
       <div class="profile">
         <img src="./src/img/yiju_profile.webp" alt="loco 프로필" />
         <div class="profile-description">
-          <p class="profile-title">name : kim</p>
-          <p class="profile-title">hobby : bicycle</p>
-          <p class="profile-title">git-hub : https://github.com/loco9939 </p>
-          <p class="profile-title">Motto : We all live in gutter. But some of us are looking at the stars.</p>
+          <p class="profile-title"><span class="title">name</span> : kim yiju</p>
+          <p class="profile-title"><span class="title">hobby</span> : riding a bicycle</p>
+          <p class="profile-title"><span class="title">git-hub</span> : https://github.com/loco9939 </p>
+          <p class="profile-title"><span class="title">Motto</span> : We all live in gutter. But some of us are looking at the stars.</p>
         </div>
       </div>
       <div class="profile">
         <div class="profile-description">
-          <p class="profile-title">name : kim</p>
-          <p class="profile-title">hobby : bicycle</p>
-          <p class="profile-title">git-hub : https://github.com/loco9939 </p>
-          <p class="profile-title">Motto : We all live in gutter. But some of us are looking at the stars.</p>
+          <p class="profile-title"><span class="title">name</span> : park jiyoon</p>
+          <p class="profile-title"><span class="title">hobby</span> : playing the piano</p>
+          <p class="profile-title"><span class="title">git-hub</span> : https://github.com/OrangeChick </p>
+          <p class="profile-title"><span class="title">Motto</span> : As if today is the last, let's do our best every day and live with no regrets.</p>
         </div>
       <img src="./src/img/jiyoon_profile.png" alt="orange 프로필" />
       </div>
       <div class="profile">
         <img src="./src/img/charyeon_profile.webp" alt="chae 프로필" />
         <div class="profile-description">
-          <p class="profile-title">name : kim</p>
-          <p class="profile-title">hobby : bicycle</p>
-          <p class="profile-title">git-hub : https://github.com/loco9939 </p>
-          <p class="profile-title">Motto : We all live in gutter. But some of us are looking at the stars.</p>
+          <p class="profile-title"><span class="title">name</span> : Lee chaeryeon</p>
+          <p class="profile-title"><span class="title">hobby</span> : climbing</p>
+          <p class="profile-title"><span class="title">git-hub</span> : https://github.com/CHAERYEON-LEE </p>
+          <p class="profile-title"><span class="title">Motto</span> : it all depends on me.</p>
         </div>
       </div>  
     </div>
